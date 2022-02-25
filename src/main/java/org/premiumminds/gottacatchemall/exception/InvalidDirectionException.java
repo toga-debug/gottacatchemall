da83216c32a4eb228c682bd8bf2d72695108fb0b
Toga <agd.santos@hotmail.com>
2022-02-25 21:41:08 +0000

    exception

---
+++ src/main/java/org/premiumminds/gottacatchemall/exception/InvalidDirectionException.java
@@ -0,0 +1,7 @@
+package org.premiumminds.gottacatchemall.exception;
+
+public class InvalidDirectionException extends RuntimeException {
+    public InvalidDirectionException(String message){
+        super(message);
+    }
+}
